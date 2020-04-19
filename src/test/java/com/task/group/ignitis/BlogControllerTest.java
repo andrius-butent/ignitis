288430062985574c15a4fcf76bf7b4bbc210fe00
Andrius.Antipovas <AAntipovas@r1rcm.com>
2020-04-19 21:14:57 +0300

    Unused import

--- src/test/java/com/task/group/ignitis/BlogControllerTest.java
+++ src/test/java/com/task/group/ignitis/BlogControllerTest.java
@@ -20,7 +20,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 
